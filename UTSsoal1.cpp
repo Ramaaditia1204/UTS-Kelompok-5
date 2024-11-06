ec3d82b99f569680760b7fe37318195da9f64d40
Ramaaditia1204 <r4m4.aditia.44@gmail.com>
2024-11-06 08:45:54 +0100

    Update UTSsoal1.cpp

--- UTSsoal1.cpp
+++ UTSsoal1.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,32 +8,32 @@ int main() {
     int golongan, jamKerja;
 
     // Input data karyawan
-    cout << "Masukkan nama karyawan: ";
+    cout << "Masukkan nama anda: ";
     getline(cin, namaKaryawan);
-    cout << "Masukkan golongan (1 - 3): ";
+    cout << "Masukkan golongan anda: ";
     cin >> golongan;
-   
+
 
     // Hitung upah pokok berdasarkan golongan
     int upahPokok;
     if (golongan == 1) {
-        upahPokok =  25000;
+        upahPokok = 25000;
     }
     else if (golongan == 2) {
-        upahPokok =  35000;
+        upahPokok = 35000;
     }
     else if (golongan == 3) {
-        upahPokok =  50000;
+        upahPokok = 50000;
     }
     else {
         cout << "Golongan tidak valid. Harap masukkan 1 - 3." << endl;
         return 0; // Keluar program dengan kode kesalahan
     }
-    
-    cout << "Masukkan jumlah jam kerja dalam seminggu: ";
+
+    cout << "Masukkan jam kerja and seminggu: ";
     cin >> jamKerja;
-   
-    int total = upahPokok* jamKerja;
+
+    int total = upahPokok * jamKerja;
 
     // Hitung uang lembur (jika ada)
     int uangLembur = 0;
@@ -45,7 +46,8 @@ int main() {
 
     // Tampilkan hasil
 
-    cout << "Total Gaji " << namaKaryawan << " Per Minggu : Rp " << gajiTotal << endl;
+    cout << " \n";
+    cout << "Total Gaji " << namaKaryawan << " Per Minggu :" << gajiTotal << endl;
 
     return 0;
 }
